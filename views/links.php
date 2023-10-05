57f298266f0b58d6961dd677cc20a1d099e2b7e3
majdsoubh53@gmail.com <majdsoubh53@gmail.com>
2023-10-05 16:42:14 +0300

    Print message when no links exist

--- views/links.php
+++ views/links.php
@@ -16,31 +16,33 @@
 
         </div>
     <?php endif ?>
+    <?php if (!empty($links)) : ?>
+        <table class="table table-responsive table-dark table-hover">
+            <thead class="table-light">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">URL</th>
+                    <th scope="col">slug</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($links as $link) : ?>
+                    <form action="<?= url('link/' . $link['id']) ?>" method="post">
+                        <tr>
+                            <th scope="row"><?= $link['id'] ?></th>
+                            <td><?= $link['url'] ?></td>
+                            <td><?= $link['slug'] ?></td>
 
-    <table class="table table-responsive table-dark table-hover">
-        <thead class="table-light">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">URL</th>
-                <th scope="col">slug</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($links as $link) : ?>
-                <form action="<?= url('link/' . $link['id']) ?>" method="post">
-                    <tr>
-                        <th scope="row"><?= $link['id'] ?></th>
-                        <td><?= $link['url'] ?></td>
-                        <td><?= $link['slug'] ?></td>
-
-                        <td><button onclick="return confirm(`Are you sure you want to remove this link`)" type='submit' class="btn btn-danger"><i class="bi bi-trash3-fill"></i></button></td>
-                    </tr>
-                    <input type="hidden" name='_method' value='DELETE'>
-                    <input type="hidden" name='_token' value=<?= csrf() ?>>
-                </form>
-            <?php endforeach ?>
-        </tbody>
-    </table>
-
+                            <td><button onclick="return confirm(`Are you sure you want to remove this link`)" type='submit' class="btn btn-danger"><i class="bi bi-trash3-fill"></i></button></td>
+                        </tr>
+                        <input type="hidden" name='_method' value='DELETE'>
+                        <input type="hidden" name='_token' value=<?= csrf() ?>>
+                    </form>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    <?php else : ?>
+        <h4>Sorry, no links exist.</h4>
+    <?php endif ?>
 </div>
